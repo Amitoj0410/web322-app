8839eb31defef4e043e0d6e9e0d0b440dc6770dc
Amitoj Singh <singhamitoj0410@gmail.com>
2022-09-30 23:46:50 -0400

    last commit

---
+++ blog-service.js
@@ -0,0 +1,100 @@
+const fs = require("fs"); 
+
+var posts = [];
+var categories = [];
+
+function initialize()
+{
+
+    return new Promise(function(resolve, reject){ // place our code inside a "Promise" function
+
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+    
+            posts = JSON.parse(data);
+            resolve("successful operation");
+
+        });
+        // console.log(posts.length);
+        // posts = posts_copy;
+        
+    
+    
+        fs.readFile('./data/categories.json', 'utf8', (err, data2) => {
+            if (err) throw err;
+            console.log(data2);
+    
+            categories = JSON.parse(data2);
+            resolve("successful operation");
+
+        });
+    
+        
+
+    });
+
+
+    
+
+}
+
+function getAllPosts()
+{
+    return new Promise(function(resolve, reject){ // place our code inside a "Promise" function
+        //console.log(posts.length);
+        if(posts.length == 0)
+        {
+            reject("no posts returned");
+            return;
+        }
+
+        resolve(posts);
+
+
+    });
+    
+}
+
+function getPublishedPosts()
+{
+    return new Promise(function(resolve, reject){ // place our code inside a "Promise" function
+        //console.log(posts.length);
+        if(posts.length == 0)
+        {
+            reject("no posts returned");
+            return;
+        }
+        for(var j = 0; j < posts.length; j++)
+        {
+            if(posts[j].published)
+            {
+                resolve(posts[j]);
+            }
+        }
+
+
+    });
+    
+}
+
+function getCategories()
+{
+    return new Promise(function(resolve, reject){ // place our code inside a "Promise" function
+        //console.log(posts.length);
+        if(categories.length == 0)
+        {
+            reject("no categories returned");
+            return;
+        }
+
+        resolve(categories);
+    });   
+}
+
+
+
+initialize().then(getCategories).then(getPublishedPosts).then(getCategories).catch(function(rejectMsg){
+    // catch any errors here
+    console.log(rejectMsg);
+});;
--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-var blog_service = require("./blog-service");
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: __Amitoj Singh____________________ Student ID: _159347210_____________ Date: _30/09/2022 (dd/mm/yyyy)_______________
+*
+*  Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+
+var blog_service = require("./blog-service.js");
 const path = require('path');
 
 var express = require("express");
@@ -23,9 +35,20 @@ app.get("/about", function(req,res){
 
 // setup another route to listen on /blog
 app.get("/blog", function(req,res){
-  //res.setHeader()
-  // res.send(JSON.stringify({key:"value"}));
-  res.send("hello");
+  res.sendFile(path.join(__dirname,"/data/posts.json"));
 });
+
+app.get("/posts", function(req,res){
+  res.sendFile(path.join(__dirname,"/data/posts.json"));
+});
+
+app.get("/categories", function(req,res){
+  res.sendFile(path.join(__dirname,"/data/categories.json"));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Error 404 ! Page Not Found");
+});
+
 
 app.listen(HTTP_PORT, onHttpStart);
