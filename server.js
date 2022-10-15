a170589843be9530ff4922acff811c1cc5a76b2b
Amitoj Singh <asingh1160@myseneca.ca>
2022-10-15 19:25:10 -0400

    111

--- server.js
+++ server.js
@@ -69,22 +69,22 @@ app.get("/posts/add", function(req,res){
 
 //
 
-app.post('/posts/add', upload.single("featureImage"), (req, res) => {
-  let streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream(
-            (error, result) => {
-            if (result) {
-                resolve(result);
-            } else {
-                reject(error);
-            }
-            }
-        );
+// app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+//   let streamUpload = (req) => {
+//     return new Promise((resolve, reject) => {
+//         let stream = cloudinary.uploader.upload_stream(
+//             (error, result) => {
+//             if (result) {
+//                 resolve(result);
+//             } else {
+//                 reject(error);
+//             }
+//             }
+//         );
 
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-};
+//         streamifier.createReadStream(req.file.buffer).pipe(stream);
+//     });
+// };
 
 
 
